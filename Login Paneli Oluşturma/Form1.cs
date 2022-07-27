b3d45950a7eac1c9354cbfb9770c084eb990389b
SznAltn <105235609+SznAltn@users.noreply.github.com>
2022-07-27 23:50:47 +0300

    Add project files.

---
+++ Login Paneli Oluşturma/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Paneli_Oluşturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
